eaaaa2ef0bdb6c2b571cca587a46b2ffe4622ff7
DaniIVT <danilvt@yandex.ru>
2026-01-12 15:39:48 +0700

    update catalog222222

--- src/App.js
+++ src/App.js
@@ -192,7 +192,9 @@ export default function App() {
   // Манифест с файлами
   const [manifest, setManifest] = useState(null);
   useEffect(() => {
-    fetch(`${process.env.PUBLIC_URL || ""}/images/manifest.json`, { cache: "no-cache" })
+    fetch(
+  `${process.env.PUBLIC_URL || ""}/images/manifest.json?v=${Date.now()}`
+)
       .then(r => r.json())
       .then(setManifest)
       .catch(() => setManifest({}));
